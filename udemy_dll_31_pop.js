ed887fad1eef4f6b9916603e7a798aaa9851eff1
Crew Spence <csspence123@gmail.com>
2020-10-01 05:52:35 -0500

    Solved toy problem.

--- udemy_dll_31_pop.js
+++ udemy_dll_31_pop.js
@@ -20,8 +20,33 @@ class Node{
 
 class DoublyLinkedList{
     constructor(val){
-        this.val = val
-        this.next = null;      
+        this.head = null;
+        this.tail = null;
+        this.length = 0;
     }
-    push(){
+    push(val){
+      let node = new Node(val);
+      if(this.length === 0) {
+        this.head = node;
+        this.tail = node;
+      } else {
+        this.tail.next = node;
+        node.prev = this.tail;
+        this.tail = node;
+      }
+      this.length++;
+      return this;
     }
+
+    pop(){
+      if(this.length === 0) {
+        return undefined;
+      }
+      let temp = this.tail;
+      this.tail = this.tail.prev;
+      this.tail.next = null;
+      temp.prev = null;
+      this.length--;
+      return temp;
+    }
+}
